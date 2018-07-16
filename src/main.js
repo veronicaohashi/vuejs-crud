0be41b6d6fe210dcb2b979685b6f3f0504f9e646
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-16 00:12:42 -0300

    Entendendo os mecanismos do main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+// Core do VUE - Global Vue Object
 import Vue from 'vue'
+// Por utilizar export default o nome do artefato importado deve
+// ser o mesmo do arquivo
 import App from './App.vue'
 
+// Vue instance, criada com o auxilio do global vue object
+// A vue instance é a ponto entre o componente e a view 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  // Objeto de configuração
+  el: '#app',                 // Elemento no qual será colocado o componente: id="app"
+  render: h => h(App)         // Função render passando o componente que será renderizado
 })
