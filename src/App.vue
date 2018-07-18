7435a096aa4cd33fa4f639b0cdc43adee7377cf7
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-18 00:23:06 -0300

    criação do painel como componente compartilhado

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,18 @@
     <!-- Não pode usar interpolação dentro de atributos -->
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-          </div>
-        </div>      
+        <meu-painel :titulo="foto.titulo">  
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+                
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
   // Função data para fornecer os dados que o template precisa
   // sempre retornando um objeto javascript no qual as propriedades deste objeto são acessíveis através
@@ -30,6 +30,10 @@ export default {
         }
       ]
     }
+  },
+  // Chave com o nome do componente que irei referenciar
+  components:{
+    'meu-painel': Painel
   }
 }
 </script>
@@ -53,29 +57,7 @@ export default {
     display: inline-block
   }
 
-  /* estilo do painel */ 
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
+  .imagem-reponsiva{
+    width: 100%;
   }
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
-}
-
-.imagem-reponsiva{
-  width: 100%;
-}
 </style>
