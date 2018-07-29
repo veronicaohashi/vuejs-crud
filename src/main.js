e1d32adea29358fa06def2aef5e11cac28c6b217
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-28 23:48:06 -0300

    Registro do routes

--- src/main.js
+++ src/main.js
@@ -12,10 +12,17 @@ Vue.use(VueResource);
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
+// Registro do routes
+import {routes} from './routes';
+// Com o auxílio do vue-router, passo as rotas configuradas e ele vai devolver um roteador que será utilizado
+// pela aplicação. propriedade routes = routes
+const router = new VueRouter({ routes });
+
 // Vue instance, criada com o auxilio do global vue object
 // A vue instance é a ponto entre o componente e a view 
 new Vue({
   // Objeto de configuração
   el: '#app',                 // Elemento no qual será colocado o componente: id="app"
+  // router,                  
   render: h => h(App)         // Função render passando o componente que será renderizado
 })
