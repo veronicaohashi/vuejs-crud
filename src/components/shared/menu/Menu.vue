8861dd121c08069fa4cd2f09d2c325b3ca3b4322
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-29 20:44:29 -0300

    Componente menu

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="route in routes">
+        <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    props: { 
+      routes:{
+        required: true,
+        type: Array
+      }
+    }   
+  }
+</script>
+
+<style>
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active{
+    transition: opacity .4s;
+  }
+</style>
