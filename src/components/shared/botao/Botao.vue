24d16fa08dc56c04f1db64f4eabdbcd29431a6e0
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-29 21:55:59 -0300

    Validação das propriedades

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -4,12 +4,18 @@
 
 <script>
   export default {
-    props: [
-      'tipo',
-      'rotulo',
-      'confirmacao',
-      'estilo'
-    ],
+    props: {
+      tipo:{
+        required: true,
+        type: String
+      },
+      rotulo:{
+        required: true,
+        type: String
+      },
+      confirmacao: Boolean,
+      estilo: String
+    },
 
     methods:{
       disparaAcao(){
