4b6c0b2b76e66bf875e782520060fbb80cb64af0
veronicaohashi <veronica.ohashi@outlook.com>
2018-07-30 23:06:22 -0300

    Adicionado formulario de cadastro

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,12 +1,95 @@
 <template>
-  <h2 class="centralizado">Cadastro</h2>
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+    <!--
+      .prevent - cancela o comportamento padrão
+      A diretiva v-model é um atalho para fazer o two way data binding
+      .lazy    - só realiza a associação de dados (da view para o modelo) quando deixar o campo
+    -->
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" v-model="foto.titulo"  >
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  },
+
+  data(){
+    return {
+      foto:{
+        titulo:'',
+        url: '',
+        descricao:''
+      }
+    }
+  },
+
+  methods:{
+    grava(){
+      console.log('enviar dados para a api');
+
+      this.foto = {
+        titulo: '',
+        url: '',
+        descricao: ''
+      }
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped>
   .centralizado{
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
     text-align: center;
   }
 </style>
