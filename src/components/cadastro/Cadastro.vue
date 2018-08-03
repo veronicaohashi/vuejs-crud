5b47f6c8d15ca4c7e1e90b2cad93cbb325f1ef24
veronicaohashi <veronica.ohashi@outlook.com>
2018-08-02 23:30:46 -0300

    Implementando método de busca para alteração

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -47,12 +47,19 @@ export default {
 
   data(){
     return {
-      foto: new Foto()
+      foto: new Foto(),
+      id:   this.$route.params.id
     }
   },
   
   created(){
     this.service = new FotoService(this.$resource);
+
+    if (this.id) {
+      this.service
+        .busca(this.id)
+        .then(foto=> this.foto = foto);
+    }
   },
 
   methods:{
