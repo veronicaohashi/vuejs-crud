06b664da3de1a621586911ff452db5ad9d9bf929
veronicaohashi <veronica.ohashi@outlook.com>
2018-08-02 23:34:53 -0300

    Implementado alteração

--- src/domain/foto/FotoService.js
+++ src/domain/foto/FotoService.js
@@ -13,8 +13,14 @@ export default class FotoService {
   }
 
   cadastra(foto) {
-    return this._resource
-      .save(foto);
+    if (foto._id){
+      return this._resource
+        .update( { id: foto._id }, foto)
+
+    } else {
+      return this._resource
+        .save(foto);
+    }
   }
 
   busca(id){
