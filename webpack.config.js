cba451b6fdf0174aa41ac61f84f0c3a0415524be
veronicaohashi <veronica.ohashi@outlook.com>
2018-08-05 21:27:16 -0300

    Adicionando loader para bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,23 @@ module.exports = {
         options: {
           name: '[name].[ext]?[hash]'
         }
-      }
+      },
+      { 
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+      },
+      { 
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream' 
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+         loader: 'file-loader'
+      },
+      { 
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+     }
     ]
   },
   resolve: {
